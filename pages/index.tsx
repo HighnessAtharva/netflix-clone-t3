680b7c78163a3713ab23ed9efea16b832117f85f
HighnessAtharva <HighnessAtharva@gmail.com>
2023-04-28 12:25:38 +0530

    complete project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,48 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import React from 'react';
+import { NextPageContext } from 'next';
+import { getSession } from 'next-auth/react';
 
-const inter = Inter({ subsets: ['latin'] })
+import Navbar from '@/components/Navbar';
+import Billboard from '@/components/Billboard';
+import MovieList from '@/components/MovieList';
+import InfoModal from '@/components/InfoModal';
+import useMovieList from '@/hooks/useMovieList';
+import useFavorites from '@/hooks/useFavorites';
+import useInfoModalStore from '@/hooks/useInfoModalStore';
 
-export default function Home() {
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+}
+
+const Home = () => {
+  const { data: movies = [] } = useMovieList();
+  const { data: favorites = [] } = useFavorites();
+  const {isOpen, closeModal} = useInfoModalStore();
+
   return (
     <>
-      <h1 className="text-4xl font-bold text-center">Hello World</h1>
+      <InfoModal visible={isOpen} onClose={closeModal} />
+      <Navbar />
+      <Billboard />
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={movies} />
+        <MovieList title="My List" data={favorites} />
+      </div>
     </>
   )
 }
+
+export default Home;
